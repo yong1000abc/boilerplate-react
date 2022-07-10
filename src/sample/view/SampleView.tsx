282aecce38930028a7a207d02f2d4cf64a3bce32
Kim Yongcheon <yong1000abc@gmail.com>
2022-07-10 13:02:44 +0900

    뷰에 version 표시

--- src/sample/view/SampleView.tsx
+++ src/sample/view/SampleView.tsx
@@ -1,15 +1,17 @@
+import {BuildEnv} from 'src/BuildEnv';
 import {useSamples} from 'src/sample/hook/useSamples';
 import styled from 'styled-components';
 
 export function SampleView() {
   const {samples} = useSamples();
-  return (
+  return <>
+    <div>{BuildEnv.version}</div>
     <List>
       {samples.map((sample, idx) => (
         <ListItem key={idx}>{sample.name}</ListItem>
       ))}
     </List>
-  );
+  </>;
 }
 
 const List = styled.ul`
